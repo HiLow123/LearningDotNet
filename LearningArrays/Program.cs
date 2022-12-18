f282ebb6cf9bd5b0bc94e65e6de492e0b64f61e8
HiLow123 <bellago@gmail.com>
2022-12-18 18:28:42 +0100

    LearningArrays added

---
+++ LearningArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bikes = {"Connondale", "Colnago", "Bianchi", "Cervelo", "Orbea"};
+            foreach  (string i in bikes)
+            {
+                Console.WriteLine(i);
+            }     
+        }
+    }
+
+    class Reverse
+    {
+        static void NotMain(string[] args)
+        {
+            int[] Numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            Array.Reverse(Numbers);
+            foreach (int value in Numbers)
+            {
+                Console.Write(value + " ");
+
+            }
+        }
+    }
+}
